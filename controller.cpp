021dd271eb39100754dfa2aac9afb2b528a383cd
Hirokazu Tomoda <hirokazu.tomoda@gmail.com>
2024-05-11 21:07:44 +0900

    wait for device to connect

--- controller.cpp
+++ controller.cpp
@@ -3,51 +3,85 @@
 #include <csignal>
 #include <chrono>
 #include <thread>
+#include <map>
+#include <vector>
+
 #include <rtmidi/RtMidi.h>
 
-bool done;
+bool done = false;
+const std::string MIDI_PORT_NAME = "UM-ONE";
 static void finish(int sig) { done = true; }
 
-int main() {
-    RtMidiIn  *midiin = 0;
-    RtMidiOut *midiout = 0;
-    std::vector<unsigned char> message;
+int findPortByName(RtMidiIn *midiin, const std::string &portName)
+{
+    unsigned int nPorts = midiin->getPortCount();
+    for (unsigned int i = 0; i < nPorts; ++i)
+    {
+        // std::cout << midiin->getPortName(i) << std::endl;
+        if (midiin->getPortName(i).find(portName) != std::string::npos)
+        {
+            return i;
+        }
+    }
+    return -1;
+}
+
+void waitForDevice(RtMidiIn *midiin, const std::string &portName)
+{
+    int portNumber = -1;
+    while ((portNumber = findPortByName(midiin, portName)) == -1)
+    {
+        std::cout << "Waiting for '" << portName << "' to connect..." << std::endl;
+        std::this_thread::sleep_for(std::chrono::seconds(5));
+    }
+    midiin->openPort(portNumber);
+    std::cout << "'" << portName << "' connected." << std::endl;
+}
+
+int main()
+{
+    RtMidiIn *midiin = nullptr;
+    RtMidiOut *midiout = nullptr;
     int noteNumber = 60; // MIDI note number to trigger fade-in
     int ccValue = 0;     // Starting value for CC
     int triggerCount = 0; // Number of times the trigger note has been caught
     bool passThrough = true; // Configure if input should be passed through
 
-    try {
+    try
+    {
         midiin = new RtMidiIn();
         midiout = new RtMidiOut();
     }
-    catch (RtMidiError &error) {
+    catch (RtMidiError &error)
+    {
         error.printMessage();
         exit(EXIT_FAILURE);
     }
 
-    // Check available ports.
-    unsigned int nPorts = midiin->getPortCount();
-    if (nPorts == 0) {
-        std::cout << "No input ports available!" << std::endl;
-        exit(EXIT_FAILURE);
-    }
+    std::signal(SIGINT, finish);
+    waitForDevice(midiin, MIDI_PORT_NAME);
 
-    midiin->openPort(0);
-    midiout->openPort(0); // Modify if your output port is different
-
-    // Don't ignore sysex, timing, or active sensing messages.
-    midiin->ignoreTypes(false, false, false);
-
-    done = false;
-    (void) signal(SIGINT, finish);
+    midiout->openVirtualPort("Controller Output"); // Open a virtual port
+    
+    midiin->ignoreTypes(false, false, false); // Don't ignore any messages
 
     // Main loop
-    while (!done) {
+    while (!done)
+    {
+        std::vector<unsigned char> message;
         midiin->getMessage(&message);
         int nBytes = message.size();
-        if (nBytes > 0) {
-            if (message[0] == 0x90 && message[1] == noteNumber && message[2] > 0) { // Note On
+        if (nBytes > 0)
+        {
+            // Example to process received messages
+            // Process more complex pattern matching and output generation here
+            std::cout << "Received MIDI data: ";
+            for (int i = 0; i < nBytes; ++i)
+            {
+                std::cout << static_cast<int>(message[i]) << " ";
+            }
+            std::cout << std::endl;
+                        if (message[0] == 0x90 && message[1] == noteNumber && message[2] > 0) { // Note On
                 if (triggerCount == 0) {
                     // Start sending CC messages for fade-in
                     while (ccValue <= 127) {
@@ -69,8 +103,7 @@ int main() {
             }
         }
 
-        // Sleep for 10ms before the next loop iteration
-        std::this_thread::sleep_for(std::chrono::milliseconds(10));
+        std::this_thread::sleep_for(std::chrono::milliseconds(10)); // Sleep to reduce CPU usage
     }
 
     delete midiin;
@@ -78,4 +111,3 @@ int main() {
 
     return 0;
 }
-
